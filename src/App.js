8b1f2a5760e2e05492740ca0a77ab62b816d0ec8
Mehakdeep Chhina <GRAZITTI\mehakdeep.chhina@mehakdeep-chhina-linux.grazitti.local>
2021-02-19 23:18:58 +0530

    cart comp

--- src/App.js
+++ src/App.js
@@ -1,49 +1,78 @@
 import React , { useState } from "react"
+import { Cart } from "./components/Cart";
 import { Filter } from "./components/Filter";
 import Products from "./components/Products"
 import data from "./data.json"
 function App() {
 const [state,setState] = useState({
   products:data.products,
+  cartItems:[],
   size:"",
   sort:"",
 })
+// console.log(state);
+const addToCart =(product) => {
+  const cartItems = state.cartItems.slice();
+  let alreadyInCart = false;
+  cartItems.forEach(item => {
+    if(item.id === product.id){
+      // item.count ? item.count ++ : item.count = 1;
+      item.count ++;
+      alreadyInCart = true;  
+    }
+  })
+  if(!alreadyInCart){
+    cartItems.push({...product, count:1})
+  }
+  setState({...state,cartItems:cartItems})
+
+
+
+}
 
 const filterProducts = (event) => {
   // console.log(event.target.value)
   if(event.target.value === "")
-    setState({
+    setState({...state,
       size: event.target.value,
       products:data.products
     })
   else
-    setState({
+    setState({...state,
       size:event.target.value,
       products: data.products.filter(product => product.availableSizes.indexOf(event.target.value) >= 0)
     })
 
 }
 const sortProducts= (event) => {
-const sort = event.target.value;
-setState({
-  sort:sort,
-  products:state.products.slice().sort((a,b) => (
-    sort === "lowest" 
-      ? 
-        ((a.price < b.price) ?
-          -1 : 1) 
-      :
-      sort === "highest" 
+  const sort = event.target.value;
+  setState({...state,
+    sort:sort,
+    products:state.products.slice().sort((a,b) => (
+      sort === "lowest" 
         ? 
-          ((a.price > b.price) 
-            ? -1 : 1) :
-              ((a._id < b._id) 
-                ? 
-                  -1 : 1) 
-    
-    
-  ))
-})
+          ((a.price < b.price) ?
+            -1 : 1) 
+        :
+        sort === "highest" 
+          ? 
+            ((a.price > b.price) 
+              ? -1 : 1) :
+                ((a._id < b._id) 
+                  ? 
+                    -1 : 1) 
+
+
+    ))
+  })
+}
+
+const removeFromCart =(product) => {
+  
+   setState({
+     ...state,
+     cartItems:state.cartItems.filter(x => (x.id !== product.id))
+  });
 }
   return (
     <div className="grid-container">
@@ -58,13 +87,20 @@ setState({
               sort={state.sort}
               filterProducts={filterProducts}
               sortProducts={(e) => sortProducts(e)}
+              
             >
 
             </Filter>
-            <Products products={state.products}></Products>
+            <Products 
+            addToCart={(product) => {addToCart(product)}}
+            products={state.products}></Products>
           </div>
           <div className="sidebar">
-            cart items
+            <Cart 
+            cartItems={state.cartItems}
+            removeFromCart={(i) => removeFromCart(i)}
+            >
+            </Cart>
           </div>
 
         </div>
