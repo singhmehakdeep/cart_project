772c92a884095c11594e0fa52b1d8e4080babe2c
Mehakdeep Chhina <GRAZITTI\mehakdeep.chhina@mehakdeep-chhina-linux.grazitti.local>
2021-02-19 13:17:16 +0530

    filter comp completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React , { useState } from "react"
+import { Filter } from "./components/Filter";
 import Products from "./components/Products"
 import data from "./data.json"
 function App() {
@@ -8,6 +9,42 @@ const [state,setState] = useState({
   sort:"",
 })
 
+const filterProducts = (event) => {
+  // console.log(event.target.value)
+  if(event.target.value === "")
+    setState({
+      size: event.target.value,
+      products:data.products
+    })
+  else
+    setState({
+      size:event.target.value,
+      products: data.products.filter(product => product.availableSizes.indexOf(event.target.value) >= 0)
+    })
+
+}
+const sortProducts= (event) => {
+const sort = event.target.value;
+setState({
+  sort:sort,
+  products:state.products.slice().sort((a,b) => (
+    sort === "lowest" 
+      ? 
+        ((a.price < b.price) ?
+          -1 : 1) 
+      :
+      sort === "highest" 
+        ? 
+          ((a.price > b.price) 
+            ? -1 : 1) :
+              ((a._id < b._id) 
+                ? 
+                  -1 : 1) 
+    
+    
+  ))
+})
+}
   return (
     <div className="grid-container">
       <header className="">
@@ -16,6 +53,14 @@ const [state,setState] = useState({
       <main>
         <div className="content">
           <div className="main">
+            <Filter count={state.products.length}
+              size={state.size}
+              sort={state.sort}
+              filterProducts={filterProducts}
+              sortProducts={(e) => sortProducts(e)}
+            >
+
+            </Filter>
             <Products products={state.products}></Products>
           </div>
           <div className="sidebar">
