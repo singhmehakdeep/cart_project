eaca972062d45e2653094194c7558d3989beac18
Mehakdeep Chhina <GRAZITTI\mehakdeep.chhina@mehakdeep-chhina-linux.grazitti.local>
2021-02-23 00:40:35 +0530

    animations

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,100 +1,104 @@
 import React, { useState } from 'react'
 import formatCurrency from './util';
+import Fade from "react-reveal/Fade"
 
 export const Cart = (props) => {
-    const {cartItems} = props;
-    const [state,setState] = useState({
-        name:"",
-        email:"",
-        address:"",
-        showCheckOut:false
+    const { cartItems } = props;
+    const [state, setState] = useState({
+        name: "",
+        email: "",
+        address: "",
+        showCheckOut: false
     })
 
     const createOrder = (e) => {
         e.preventDefault();
-        const order={
-            name:state.name,
-            email:state.email,
-            address:state.address,
-            cartItems:cartItems,
+        const order = {
+            name: state.name,
+            email: state.email,
+            address: state.address,
+            cartItems: cartItems,
         }
         props.createOrder(order);
 
     }
 
-    const handleChange =(e) => {
-        setState({...state,[e.target.name]:e.target.value})
+    const handleChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value })
 
     }
 
-    
+
     return (
         <>
-        <div>
-            {cartItems.length === 0 ? <div className="cart cart-header">Cart is Empty</div>
-            :
-            <div className="cart cart-header">You have {cartItems.length} in the cart</div>
-            }
-        </div>
-        <div>
-            <div className="cart">
-                <ul className="cart-items">
-                    {cartItems.map((item)=> (
-                        <li key={item.id}>
-                            <div>
-                                <img src={item.image} alt={cartItems.title}></img>
-                            </div>
-                            <div>
-                                <div>{item.title}</div>
-                    <div className="right">{formatCurrency(item.price)} x {item.count} {" "}</div>
-                                <button onClick={() => props.removeFromCart(item)}>
-                                    Remove
-                                </button>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            {cartItems.length !==0 && (
             <div>
-            <div className="cart">
-                <div className="total">
-                    <div>
-                        {formatCurrency(cartItems.reduce((a,b) => a + b.price * b.count,0) )}
-                    </div>
-                    <button className="primary button" onClick={() => setState({showCheckOut:true})}>
-                        Proceed
-                    </button>
-                </div>
+                {cartItems.length === 0 ? <div className="cart cart-header">Cart is Empty</div>
+                    :
+                    <div className="cart cart-header">You have {cartItems.length} in the cart</div>
+                }
             </div>
-            {state.showCheckOut && (
+            <div>
                 <div className="cart">
-                    <form onSubmit={createOrder}>
-                        <ul className="form-container">
-                            <li>
-                                <label>Email</label>
-                                <input name="email" type="email" required onChange={handleChange}></input>
-                            </li>
-                            <li>
-                                <label>Name</label>
-                                <input name="name" type="text" required onChange={handleChange}></input>
-                            </li>
-                            <li>
-                                <label>Address</label>
-                                <input name="address" type="text" required onChange={handleChange}></input>
-                            </li>
-                            <li>
-                                <button className="button primary" type="submit">Checkout</button>
-                            </li>
+                    <Fade left cascade>
+                        <ul className="cart-items">
+                            {cartItems.map((item) => (
+                                <li key={item.id}>
+                                    <div>
+                                        <img src={item.image} alt={cartItems.title}></img>
+                                    </div>
+                                    <div>
+                                        <div>{item.title}</div>
+                                        <div className="right">{formatCurrency(item.price)} x {item.count} {" "}</div>
+                                        <button onClick={() => props.removeFromCart(item)}>
+                                            Remove
+                                </button>
+                                    </div>
+                                </li>
+                            ))}
                         </ul>
-                    </form>
-
+                    </Fade>
                 </div>
-            )}
+                {cartItems.length !== 0 && (
+                    <div>
+                        <div className="cart">
+                            <div className="total">
+                                <div>
+                                    {formatCurrency(cartItems.reduce((a, b) => a + b.price * b.count, 0))}
+                                </div>
+                                <button className="primary button" onClick={() => setState({ showCheckOut: true })}>
+                                    Proceed
+                    </button>
+                            </div>
+                        </div>
+                        {state.showCheckOut && (
+                            <div className="cart">
+                                <Fade right cascade>
+                                    <form onSubmit={createOrder}>
+                                        <ul className="form-container">
+                                            <li>
+                                                <label>Email</label>
+                                                <input name="email" type="email" required onChange={handleChange}></input>
+                                            </li>
+                                            <li>
+                                                <label>Name</label>
+                                                <input name="name" type="text" required onChange={handleChange}></input>
+                                            </li>
+                                            <li>
+                                                <label>Address</label>
+                                                <input name="address" type="text" required onChange={handleChange}></input>
+                                            </li>
+                                            <li>
+                                                <button className="button primary" type="submit">Checkout</button>
+                                            </li>
+                                        </ul>
+                                    </form>
+                                </Fade>
+
+                            </div>
+                        )}
+                    </div>
+                )}
             </div>
-            )}
-        </div>
         </>
     )
 }
- 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,31 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
 import formatCurrency from './util'
+import Fade from "react-reveal/Fade"
+import Zoom from "react-reveal/Zoom"
+import Modal from "react-modal"
 
 export default function Products(props) {
+    const [modalState, modalSetState] = useState(null)
+
+    const openModal = (product) => {
+        modalSetState(product);
+    }
+
+    const closeModal = () => {
+        modalSetState(null);
+    }
+
+
     return (
         <div>
-            <ul className="products">
-                {props.products.map(product => 
-                <li key={product._id}>
-                    <div className="product">
-                        <a href={"#" + product._id}>
-                            <img src={product.image} alt={product.title}/>
-                            <p>
-                                {product.title}
-                            </p>    
-                        </a>
-                        <div className="product-price">
-                        <div>
-                            {formatCurrency(product.price)}
-                        </div>
-                        <button onClick={() => props.addToCart(product)} className="button primary">
-                            Add to cart
+            <Fade bottom cascade>
+                <ul className="products">
+                    {props.products.map(product =>
+                        <li key={product._id}>
+                            <div className="product">
+                                <a href={"#" + product._id} onClick={() => openModal(product)}>
+                                    <img src={product.image} alt={product.title} />
+                                    <p>
+                                        {product.title}
+                                    </p>
+                                </a>
+                                <div className="product-price">
+                                    <div>
+                                        {formatCurrency(product.price)}
+                                    </div>
+                                    <button onClick={() => props.addToCart(product)} className="button primary">
+                                        Add to cart
                         </button>
-                        </div>
-                    </div>
+                                </div>
+                            </div>
 
-                </li>)}
-            </ul>
+                        </li>)}
+                </ul>
+            </Fade>
+            {modalState && (
+                <Modal isOpen={true} onRequestClose={closeModal}>
+                    <Zoom>
+                        <div>Modal</div>
+                        <button onClick={closeModal} className="close-modal">X</button>
+                        <div className="product-details">
+                            <img src={modalState.image} alt={modalState.title}></img>
+                            <div className="product-details-description">
+                                <p>
+                                    <strong>{modalState.title}</strong>
+                                </p>
+                                <p>
+                                    {modalState.description}
+                                </p>
+                                <p>
+                                    Available Sizes {" "}
+                                    {modalState.availableSizes.map(x => (
+                                        <span>
+                                            {" "}
+                                    <button className="button ">{x}</button>
+                                        </span>
+                                    ))}
+                                </p>
+                                <div className="product-price">
+                                    <div>{formatCurrency(modalState.price)}</div>
+                                    <button
+                                    className="button primary"
+                                    onClick={() => {
+                                        props.addToCart(modalState);
+                                        closeModal();
+                                    }}
+                                    >
+                                        Add to Cart
+                                    </button>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </Zoom>
+                </Modal>
+            )}
+
         </div>
     )
 }
